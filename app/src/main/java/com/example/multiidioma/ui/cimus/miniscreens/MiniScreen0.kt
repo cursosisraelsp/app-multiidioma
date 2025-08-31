4a4b0fab1dcfd258a0c8cf77dd18038dcd3fa25e
cursosisraelsp <cursos.israelsp@gmail.com>
2025-08-31 09:35:17 +0200

    engadindo aspecto o vídeo e color branco o texto en MiniScreen0

--- app/src/main/java/com/example/multiidioma/ui/cimus/miniscreens/MiniScreen0.kt
+++ app/src/main/java/com/example/multiidioma/ui/cimus/miniscreens/MiniScreen0.kt
@@ -1,11 +1,16 @@
 package com.example.multiidioma.ui.cimus.miniscreens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.multiidioma.data.MiniScreenData
 import com.example.multiidioma.ui.LocalizedContext
@@ -20,15 +25,27 @@ fun MiniScreen0(
     // Usa el contexto localizado que ya pusiste en CompositionLocalProvider
     val context = LocalizedContext.current
 
-    Column(modifier = modifier.padding(16.dp)) {
-        data.bodyParagraphs.forEach { res ->
-            Text(
-                text =  runCatching { context.getString(res) }.getOrElse { "???" },
-                style = MaterialTheme.typography.bodyMedium
-            )
+    Box{
+        Column(modifier = modifier.padding(16.dp).background(color = Color.Transparent)) {
+            Box{
+                VideoScreen(videoId = "dQw4w9WgXcQ", // tu ID de video
+                    modifier = Modifier.fillMaxSize().aspectRatio(16f / 9f))
+            }
+            Box(modifier = modifier.background(color = Color.Transparent)){
+                data.bodyParagraphs.forEach { res ->
+                    Text(
+                        text =  runCatching { context.getString(res) }.getOrElse { "???" },
+                        color = Color.White,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+            }
+
+
+
         }
-        VideoScreen(videoId = "dQw4w9WgXcQ", // tu ID de video
-            modifier = Modifier)
     }
+
+
 }
 
