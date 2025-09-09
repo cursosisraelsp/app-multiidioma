cfe43d4970809a8d624e9a5af979c5c84f26c347
cursosisraelsp <cursos.israelsp@gmail.com>
2025-09-09 13:05:08 +0200

    ...

--- app/src/main/java/com/example/multiidioma/ui/screens/multimedia/MultimediaScreen.kt
+++ app/src/main/java/com/example/multiidioma/ui/screens/multimedia/MultimediaScreen.kt
@@ -1,38 +1,15 @@
 package com.example.multiidioma.ui.screens.multimedia
 
-import android.content.Intent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import com.example.multiidioma.R
-import android.net.Uri
-import androidx.compose.foundation.border
-import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.carousel.HorizontalMultiBrowseCarousel
-import androidx.compose.material3.carousel.HorizontalUncontainedCarousel
 import androidx.compose.material3.carousel.rememberCarouselState
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.multiidioma.data.repository.MultimediaRepository
 import com.example.multiidioma.data.types.CarruselMultimediaData
 import com.example.multiidioma.ui.components.CarruselMultimedia
-import com.example.multiidioma.ui.components.OpenSpotify
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
