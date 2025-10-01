30f9a96840db99ee94ad0f403bf4c21df25e982c
cursosisraelsp <cursos.israelsp@gmail.com>
2025-10-01 22:41:03 +0200

    primer commmit

---
+++ app/src/main/java/com/example/multiidioma/ui/screens/ipsius/ipsiusMiniScreens/ipsiusMiniScreen19/IpsiusMiniScreen19.kt
@@ -0,0 +1,38 @@
+package com.example.multiidioma.ui.screens.institutes.ipsius_andre.ipsiusMiniScreens.ipsiusMiniScreen19
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.example.multiidioma.data.BOX
+import com.example.multiidioma.data.types.MiniScreenData
+import com.example.multiidioma.ui.components.Templates.TemplateScreen
+import com.example.multiidioma.ui.screens.institutes.ipsius_andre.ipsiusMiniScreens.ipsiusMiniScreen19.contentIpsiusMiniScreen19.contentIpsiusMiniScreen19Box1
+import com.example.multiidioma.ui.screens.institutes.ipsius_andre.ipsiusMiniScreens.ipsiusMiniScreen19.contentIpsiusMiniScreen19.contentIpsiusMiniScreen19Box2
+import com.example.multiidioma.ui.screens.institutes.ipsius_andre.ipsiusMiniScreens.ipsiusMiniScreen19.contentIpsiusMiniScreen19.contentIpsiusMiniScreen19Box3
+
+
+@Composable
+fun IpsiusMiniScreen19(
+    data: MiniScreenData,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xFF32627E))
+    ) {
+        TemplateScreen(
+            BoxWeight = BOX(Box1 = 0.35f, Box2 = 0.40f, Box3 = 0.25f),
+            data = data,
+            Box1 = { contentIpsiusMiniScreen19Box1(it) },
+            Box2 = { contentIpsiusMiniScreen19Box2(it) },
+            Box3 = { contentIpsiusMiniScreen19Box3() })
+    }
+}
+
+
+
---
+++ app/src/main/java/com/example/multiidioma/ui/screens/ipsius/ipsiusMiniScreens/ipsiusMiniScreen29/IpsiusMiniScreen29.kt
@@ -0,0 +1,39 @@
+package com.example.multiidioma.ui.screens.institutes.ipsius_andre.ipsiusMiniScreens.ipsiusMiniScreen29
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavController
+import com.example.multiidioma.data.types.MiniScreenData
+import com.example.multiidioma.ui.components.Templates.PersonalTemplateScreen
+import com.example.multiidioma.ui.components.Templates.TemplateScreen
+import com.example.multiidioma.ui.screens.institutes.ipsius_andre.ipsiusMiniScreens.ipsiusMiniScreen20i.contentIpsiusMiniScreen20.contentIpsiusMiniScreen20Box1
+import com.example.multiidioma.ui.screens.institutes.ipsius_andre.ipsiusMiniScreens.ipsiusMiniScreen20i.contentIpsiusMiniScreen20.contentIpsiusMiniScreen20Box2
+import com.example.multiidioma.ui.screens.institutes.ipsius_andre.ipsiusMiniScreens.ipsiusMiniScreen20i.contentIpsiusMiniScreen20.contentIpsiusMiniScreen20Box3
+import com.example.multiidioma.ui.screens.institutes.ipsius_andre.ipsiusMiniScreens.ipsiusMiniScreen29.contentIpsiusMiniScreen29.contentIpsiusMiniScreen29Box2
+import com.example.multiidioma.ui.screens.institutes.ipsius_andre.ipsiusMiniScreens.ipsiusMiniScreen29.contentIpsiusMiniScreen29.contentIpsiusMiniScreen29Box3
+
+
+@Composable
+fun IpsiusMiniScreen29(
+    data: MiniScreenData,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xFF4189B5))
+    ) {
+        TemplateScreen(
+            data = data,
+            Box2 = { contentIpsiusMiniScreen29Box2(it) },
+            Box3 = { contentIpsiusMiniScreen29Box3(it) })
+
+    }
+}
+
+
