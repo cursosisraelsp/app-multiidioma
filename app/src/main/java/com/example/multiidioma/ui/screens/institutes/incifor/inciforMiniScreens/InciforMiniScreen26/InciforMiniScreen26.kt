fcca0b39c77eec3d5b60ca0b371494fa7a8689f7
ElKikeDeVerdad <jsaaher.genius@gmail.com>
2025-10-10 14:22:29 +0200

    ..Ãlisto lazyrow, falta quitar colores o cambiarlos

--- app/src/main/java/com/example/multiidioma/ui/screens/institutes/incifor/inciforMiniScreens/InciforMiniScreen26/InciforMiniScreen26.kt
+++ app/src/main/java/com/example/multiidioma/ui/screens/institutes/incifor/inciforMiniScreens/InciforMiniScreen26/InciforMiniScreen26.kt
@@ -8,14 +8,21 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -29,6 +36,7 @@ import com.example.multiidioma.ui.screens.incifor.inciforMiniScreens.InciforMini
 import com.example.multiidioma.ui.screens.institutes.incifor.inciforMiniScreens.InciforMiniScreen26.contentInciforMiniScreen.contentFirstDirectiveTextDescriptionMiniScreen26
 import com.example.multiidioma.ui.screens.institutes.incifor.inciforMiniScreens.InciforMiniScreen26.contentInciforMiniScreen.contentSecondDirectiveImageMiniScreen26
 import com.example.multiidioma.ui.screens.institutes.incifor.inciforMiniScreens.InciforMiniScreen26.contentInciforMiniScreen.contentSecondDirectiveTextDescriptionMiniScreen26
+import com.example.multiidioma.utils.TextTitleMedium
 
 @Composable
 fun InciforMiniScreen26(
@@ -37,81 +45,110 @@ fun InciforMiniScreen26(
     podcasts: List<Podcast>,
     modifier: Modifier = Modifier
 ) {
+    val investigadores = data.personalResearcher ?: emptyList()
+
+    // Separar Directora y Secretaria
+    val principales = investigadores.filter {
+        it.name == "Ana María Bermejo Barrera" || it.name == "Inés Sánchez Sellero"
+    }
+
+    // Resto de investigadores
+    val resto = investigadores.filter { it !in principales }
+
     Column(
-        modifier =
-            Modifier
-                .fillMaxSize()
-                .background(Color.Blue)
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF4189B5))
     ) {
         Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .weight(0.4f)
-                /*.background(Color(0xFF32627E))*/.background(
-                    Color.Red
-                )
-
+            /*.background(Color.Red)*/
         ) {
-            val lista = listOf<@Composable ((MiniScreenData) -> Unit)>(
-                { contentFirstDirectiveImageMiniScreen26(it) },
-                { contentFirstDirectiveTextDescriptionMiniScreen26(it) },
-                { contentSecondDirectiveImageMiniScreen26(it) },
-                { contentSecondDirectiveTextDescriptionMiniScreen26(it) }
-            )
-            val elementosLazyRowTemplate = WorkersCenter(
-                infoWorkerCenter = lista,
-                podcast = podcasts
-            )
-            Text(text = stringResource(data.bodyParagraphs[0]))
-        }
-        LazyRow(
-            modifier = modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.4f)
-                .background(Color.Green),
-            contentPadding = PaddingValues(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.spacedBy(12.dp)
-        ) {
-            items(data.personalResearcher!!) { researcher ->
-                Column(
-                    modifier = Modifier.clickable {
-                        val ruta = "detalles/incifor/${researcher.id}"
-
-                        //navController.navigate(researcher.ruta)
-                        navController.navigate(ruta)
-                    }
-                ) {
-                    val ruta = "${researcher.ruta}/incifor/${researcher.id}"
-
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .background(Color.Gray)
-                            .weight(0.3f)
-                    ) {
-                        Image(
-                            painter = painterResource(researcher.foto),
-                            contentDescription = "foto",
-                            contentScale = ContentScale.FillBounds
-                        )
-                    }
+            LazyRow(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(400.dp)
+                    .padding(horizontal = 16.dp),
+                horizontalArrangement = Arrangement.spacedBy(12.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                items(principales) { investigador ->
                     Column(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .weight(0.3f)
-                            .background(Color.DarkGray)
+                            .height(300.dp) //tamaño cuadrado del personal alto
+                            .width(250.dp)  //tamaño cuadrado del personal ancho
+                            .clip(RoundedCornerShape(8.dp))
+                            .background(Color.Yellow)
+                            .clickable {
+                                val ruta = "detalles/incifor/${investigador.id}"
+                                navController.navigate(ruta)
+                            }
+                            .padding(8.dp)
                     ) {
-                        Text("### A ruta é ###")
-                        Text("${researcher.ruta}")
-                        Text("······O id é····")
-                        Text("${researcher.id}")
-                        Text("····A RUTA CONCACT")
-                        Text("$ruta")
+                        Image(
+                            painter = painterResource(investigador.foto),
+                            contentDescription = investigador.name ?: "Sin nombre",
+                            contentScale = ContentScale.Fit,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(150.dp) //alto imagen
+                                .clip(RoundedCornerShape(8.dp))
+                        )
+                        Spacer(Modifier.height(8.dp))
+                        Text(text = investigador.name ?: "Sin nombre", color = Color.White)
+                        investigador.info?.forEach { resId ->
+                            resId?.let { Text(text = stringResource(it), color = Color.LightGray) }
+                        }
                     }
                 }
             }
         }
 
-
+        // Parte inferior
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(350.dp)
+            /*.background(Color.Green)*/
+        ) {
+            LazyRow(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp),
+                horizontalArrangement = Arrangement.spacedBy(12.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                items(resto) { investigador ->
+                    Column(
+                        modifier = Modifier
+                            .height(325.dp)// alto tarjeta investigadores
+                            .width(225.dp) // ancho tarjeta investigadores
+                            .clip(RoundedCornerShape(8.dp))
+                            .background(Color.DarkGray)
+                            .clickable {
+                                val ruta = "detalles/incifor/${investigador.id}"
+                                navController.navigate(ruta)
+                            }
+                            .padding(8.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(investigador.foto),
+                            contentDescription = investigador.name ?: "Sin nombre",
+                            contentScale = ContentScale.Fit,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(125.dp)
+                                .clip(RoundedCornerShape(8.dp))
+                        )
+                        Spacer(Modifier.height(8.dp))
+                        Text(text = investigador.name ?: "Sin nombre", color = Color.White)
+                        investigador.info?.forEach { resId ->
+                            resId?.let { Text(text = stringResource(it), color = Color.LightGray) }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
