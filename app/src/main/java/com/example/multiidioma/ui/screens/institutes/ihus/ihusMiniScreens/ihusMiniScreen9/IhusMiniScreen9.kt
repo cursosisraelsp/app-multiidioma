734d4b45d8605fbca8abeace4cd1304e7e96bc87
cursosisraelsp <cursos.israelsp@gmail.com>
2025-10-13 20:36:29 +0200

    cambio de nomes

--- app/src/main/java/com/example/multiidioma/ui/screens/institutes/ihus/ihusMiniScreens/ihusMiniScreen9/IhusMiniScreen9.kt
+++ app/src/main/java/com/example/multiidioma/ui/screens/institutes/ihus/ihusMiniScreens/ihusMiniScreen9/IhusMiniScreen9.kt
@@ -9,9 +9,6 @@ import androidx.compose.ui.graphics.Color
 import com.example.multiidioma.data.BOX
 import com.example.multiidioma.data.types.MiniScreenData
 import com.example.multiidioma.ui.components.Templates.TemplateScreen
-import com.example.multiidioma.ui.screens.ihus.ihusMiniScreens.ihusMiniScreen8.contentIhusMiniScreen8.contentIhusMiniScreen8Box1
-import com.example.multiidioma.ui.screens.ihus.ihusMiniScreens.ihusMiniScreen8.contentIhusMiniScreen8.contentIhusMiniScreen8Box2
-import com.example.multiidioma.ui.screens.ihus.ihusMiniScreens.ihusMiniScreen8.contentIhusMiniScreen8.contentIhusMiniScreen8Box3
 import com.example.multiidioma.ui.screens.ihus.ihusMiniScreens.ihusMiniScreen9.contentIhusMiniScreen9.contentIhusMiniScreen9Box1
 import com.example.multiidioma.ui.screens.ihus.ihusMiniScreens.ihusMiniScreen9.contentIhusMiniScreen9.contentIhusMiniScreen9Box2
 import com.example.multiidioma.ui.screens.ihus.ihusMiniScreens.ihusMiniScreen9.contentIhusMiniScreen9.contentIhusMiniScreen9Box3
@@ -25,14 +22,14 @@ fun IhusMiniScreen9(
     Box(
         modifier = modifier
             .fillMaxSize()
-            .background(Color(0xFF4189B5))
+            .background(Color(0xFF32627E))
     ) {
         TemplateScreen(
-            BoxWeight = BOX(Box1 = 0.2f, Box2 = 0.5f, Box3 = 0.3f),
+            BoxWeight = BOX(Box1 = 0.1f, Box2 = 0.6f, Box3 = 0.3f),
             data = data,
             Box1 = { contentIhusMiniScreen9Box1(it) },
             Box2 = { contentIhusMiniScreen9Box2(it)  },
-            Box3 = { contentIhusMiniScreen9Box3()  },
+            Box3 = { contentIhusMiniScreen9Box3(it)  },
         )
     }
 }
